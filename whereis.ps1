a26ccfc57670858e2d798c6f23168eb359545bb6
gzj <gzj00@outlook.com>
2023-08-15 17:04:38 +0800

    add whereis script.

---
+++ whereis.ps1
@@ -0,0 +1,9 @@
+function whereis {
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory=$true, Position=0)]
+        [string]$CommandName
+    )
+    
+    Get-Command -Name $CommandName -ShowCommandInfo
+}
