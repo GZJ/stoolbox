247a7a39df084017af829e839ad8874d501d0c71
gzj <gzj00@outlook.com>
2023-08-15 16:26:07 +0800

    add exit functionality to sound_volume function.

--- sound_volume.ps1
+++ sound_volume.ps1
@@ -1,6 +1,10 @@
 function sound_volume {
     while ($true) {
         $key = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        if ($key.VirtualKeyCode -eq 0x0D) {
+            break
+        }
+
         if ($key.VirtualKeyCode -eq 0x4A) {
             Write-Host "volume down"
             (New-Object -com wscript.shell).SendKeys([char]174)
