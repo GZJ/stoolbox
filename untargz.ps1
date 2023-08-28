dd98bab436d50693a32915114d40283f3c5614fc
gzj <gzj00@outlook.com>
2023-08-28 08:28:52 +0800

    add untargz and untarxz.

---
+++ untargz.ps1
@@ -0,0 +1,23 @@
+function untargz {
+    [CmdletBinding()]
+    param(
+        [string]$filePath,
+        [string]$outputPath = (Get-Location)
+    )
+
+    if (-not (Test-Path $filePath)) {
+        Write-Host "File not found: $filePath"
+        return
+    }
+    $tarName = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
+    $fileName = $tarName -replace '\.tar$',''
+    $outputPath = Join-Path $outputPath $fileName
+    $outputTarPath = Join-Path $outputPath $tarName
+
+    New-Item -ItemType Directory -Path $outputPath -ErrorAction SilentlyContinue | Out-Null
+
+    7z.exe x -Y $filePath -o"$outputPath" | Out-Null
+    7z.exe x -Y $outputTarPath -o"$outputPath" | Out-Null
+
+    Remove-Item $outputTarPath
+}
