8f3c49d60ed92978ba040adeb635087868187383
gzj <gzj00@outlook.com>
2023-09-14 21:27:55 +0800

    enhance gpaste with formatted output.

--- gpaste.ps1
+++ gpaste.ps1
@@ -5,7 +5,13 @@ function gpaste {
             ValueFromRemainingArguments = $true,
             HelpMessage = "Specify the destination path(s) to copy."
         )]
-        [string[]]$DstPath = $PWD
+        [string[]]$DstPath = $PWD,
+
+        [Parameter(
+            HelpMessage = "Specify the format for returned items. Options: 'text', 'newline', 'comma', 'json'. Default is 'text'."
+        )]
+        [ValidateSet("text","newline","comma","json")]
+        [string]$Format = "text"
     )
 
     if (-not ("System.Windows.Forms.Clipboard" -as [type])) {
@@ -13,10 +19,22 @@ function gpaste {
     }
     $items = [System.Windows.Forms.Clipboard]::GetFileDropList()
 
+    $result = @()
+
     foreach ($p in $DstPath) {
         foreach ($item in $items) {
             Copy-Item -Recurse $item -Destination $p -Force
-            Write-Host "Copied file '$item' to '$p'"
+            if ($Format -eq "text") {
+                $result += "Copied file '$item' to '$p'"
+            } else {
+                $result += $item
+            }
         }
+    }
+    switch ($Format) {
+        "text" { return $result -join "`n" }
+        "newline" { return $result -join "`n" }
+        "comma" { return "$(($result | ForEach-Object { '"' + $_ + '"' }) -join ',')" }
+        "json" { return $result | ConvertTo-Json }
     }
 }
