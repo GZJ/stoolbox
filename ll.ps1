cdb5eba14e603cc5346bf56d318dd574f34a76f6
gzj <gzj00@outlook.com>
2023-12-12 21:52:07 +0800

    ll function - display file sizes in a human-readable format.

---
+++ ll.ps1
@@ -0,0 +1,25 @@
+function ll {
+    Get-ChildItem | ForEach-Object {
+        $size = $_.Length
+        $sizeUnit = "Bytes"
+
+        if ($size -ge 1GB) {
+            $size = $size / 1GB
+            $sizeUnit = "GB"
+        }
+        elseif ($size -ge 1MB) {
+            $size = $size / 1MB
+            $sizeUnit = "MB"
+        }
+        elseif ($size -ge 1KB) {
+            $size = $size / 1KB
+            $sizeUnit = "KB"
+        }
+
+        [PSCustomObject]@{
+            LastWriteTime = $_.LastWriteTime
+            Size = "{0:N2} {1}" -f $size, $sizeUnit
+            Name = $_.Name
+        }
+    } | Format-Table
+}
