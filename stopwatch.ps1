ed98b4151ecba6d2ac611fb91f9c3239f4cdf717
gzj <gzj00@outlook.com>
2024-01-22 21:00:13 +0800

    feat(ps1): add stopwath.ps1

---
+++ stopwatch.ps1
@@ -0,0 +1,17 @@
+function StopWatch {
+    $stopwatch = [System.Diagnostics.Stopwatch]::new()
+    $stopwatch.Start()
+
+    while ($true) {
+        if ([System.Console]::KeyAvailable) {
+            $key = [System.Console]::ReadKey($true)
+            if ($key.Key -eq "Enter") {
+                $stopwatch.Stop()
+                break
+            }
+        } else {
+            $elapsedTime = $stopwatch.Elapsed
+            Write-Host -NoNewLine "`r$($elapsedTime.Hours):$($elapsedTime.Minutes):$($elapsedTime.Seconds).$($elapsedTime.Milliseconds)"
+        }
+    }
+}
