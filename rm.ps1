ea843e874c481cbe168d0d598611b4d21d5a55d0
gzj <gzj00@outlook.com>
2024-10-28 16:45:12 +0800

    feat: add rm.ps1

---
+++ rm.ps1
@@ -0,0 +1,25 @@
+function rm {
+    param (
+        [Parameter(ValueFromRemainingArguments = $true)]
+        [string[]]$Items
+    )
+    
+    $shell = New-Object -ComObject Shell.Application
+    
+    $Items | ForEach-Object {
+        $item = $_
+        $fullPath = Resolve-Path $item -ErrorAction SilentlyContinue
+        
+        if ($fullPath) {
+            $folder = $shell.Namespace((Split-Path $fullPath -Parent))
+            $itemToMove = $folder.ParseName((Split-Path $fullPath -Leaf))
+            
+            if ($itemToMove) {
+                $itemToMove.InvokeVerb('delete')
+                Write-Host "Moved to recycle bin: $fullPath" -ForegroundColor Green
+            }
+        } else {
+            Write-Host "Path '$item' does not exist" -ForegroundColor Yellow
+        }
+    }
+}
